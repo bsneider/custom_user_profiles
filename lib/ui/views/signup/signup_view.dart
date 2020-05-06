410a910abba8a72c63968d9d9b7531748710d1a2
Brandon Sneider <54788551+bsneider@users.noreply.github.com>
2020-05-05 20:50:35 -0500

    add gesture detector so can click off the box and keyboard goes away

--- lib/ui/views/signup/signup_view.dart
+++ lib/ui/views/signup/signup_view.dart
@@ -12,31 +12,35 @@ import 'package:compound/forms/user_form.dart';
 import 'package:compound/ui/shared/app_colors.dart';
 
 class SignUpView extends StatelessWidget {
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-  final fullNameController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<SignUpViewModel>.reactive(
       viewModelBuilder: () => SignUpViewModel(),
-      builder: (context, model, child) => Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 50.0),
-          child: UserForm(),
-        ),
-        appBar: AppBar(
-          backgroundColor: primaryColor,
-          elevation: 0.0,
-          actions: <Widget>[
-            FlatButton.icon(
-                onPressed: () async {
-                  model.navigateToLogin();
-                },
-                icon: Icon(Icons.person),
-                label: Text('Login'))
-          ],
-        ),
-      ),
+      builder: (context, model, child) => GestureDetector(
+          onTap: () {
+            FocusScopeNode currentFocus = FocusScope.of(context);
+            if (!currentFocus.hasPrimaryFocus) {
+              currentFocus.unfocus();
+            }
+          },
+          child: Scaffold(
+            body: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50.0),
+              child: UserForm(),
+            ),
+            appBar: AppBar(
+              backgroundColor: primaryColor,
+              elevation: 0.0,
+              actions: <Widget>[
+                FlatButton.icon(
+                    onPressed: () async {
+                      model.navigateToLogin();
+                    },
+                    icon: Icon(Icons.person),
+                    label: Text('Login'))
+              ],
+            ),
+          )),
     );
   }
 }
